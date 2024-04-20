894ea9e24c279e8fe9e7bb64bd2a4223fde2077d
Muhammad Farid Yamin <muhammad.farid.yamin@gmail.com>
2024-04-20 23:12:48 +0700

    fixing input fields, adding icon to logo, adding more style to input and button

---
+++ script.js
@@ -0,0 +1,10 @@
+// const inputs = document.getElementsByTagName("input");
+// [...inputs].forEach((input) => {
+//   input.addEventListener("input", () => {
+//     if (input.value.trim() === "") {
+//       input.classList.remove("placeholder-on");
+//     } else {
+//       input.classList.add("placeholder-on");
+//     }
+//   });
+// });
